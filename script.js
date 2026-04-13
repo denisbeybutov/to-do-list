6824817877114914e3a3d6ce96bd550fe47c0eb5
denisbeybutov <beybutov.denis@mail.ru>
2026-04-13 14:26:30 +0300

    сделано редактирование и удаление заметок

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let valueOfNote;
+
 // зачеркивает текст когда чекбокс активен
 document.querySelectorAll('.list__checkbox').forEach(check => {
     check.addEventListener('change', function(){
@@ -8,17 +10,25 @@ document.querySelectorAll('.list__checkbox').forEach(check => {
 // удаление заметок по кнопке корзины
 document.querySelectorAll('.delete').forEach(function(trash) {
     trash.addEventListener('click', function(){
-        console.log('delete', trash.parentElement.parentElement)
-        trash.parentElement.parentElement.remove();
+        const currentTrash = trash.parentElement.parentElement;
+        currentTrash.remove();
+        console.log( )
+        const countOfNotes = document.querySelectorAll('.list__item').length;
+        if (countOfNotes === 0) {
+            document.querySelector('.empty').classList.remove('hidden');
+        }
+
     })
 })
 
-// редактирование заметки
+// начать редактирование заметки
 document.querySelectorAll('.edit').forEach(function(ed) {
     ed.addEventListener('click', function(){
         
         const input = ed.parentElement.parentElement.querySelector('.list__input-text')
         const length = input.value.length;
+        valueOfNote = input.value;
+        
         input.removeAttribute('disabled')
         input.focus();
         input.setSelectionRange(length, length)
@@ -26,8 +36,51 @@ document.querySelectorAll('.edit').forEach(function(ed) {
         ed.parentElement.classList.add('hidden')
         ed.parentElement.nextElementSibling.classList.remove('hidden')
         
+        // запретить редактировать другие заметки пока не отредактировали выбранную
+        const currentList = ed.parentElement.parentElement;
+        document.querySelectorAll('.list__item').forEach(function(list){
+            if (list !== currentList) {
+                list.querySelector('.list__wrapper-change ').classList.add('hidden')
+            }
+        })
     })
 })
 
-// принять редактирование
-document.querySelectorAll
+// закончить редактирование и сохранить изменения
+document.querySelectorAll('.end-edit').forEach(function(item){
+    item.addEventListener('click', function(){
+        
+        item.parentElement.previousElementSibling.classList.remove('hidden')
+        item.parentElement.classList.add('hidden')
+        
+        item.parentElement.parentElement.querySelector('.list__input-text').setAttribute('disabled','')
+
+        // разрешить редактировать другие заметки
+        allowEditingOfNotes(item);
+    })
+})
+
+// отклонить редактирование и сбросить редактирование
+
+document.querySelectorAll('.reset').forEach(function(res){
+    res.addEventListener('click', function(){
+        
+        res.parentElement.parentElement.querySelector('.list__input-text').value = valueOfNote;
+        res.parentElement.parentElement.querySelector('.list__input-text').setAttribute('disabled', '');
+        res.parentElement.classList.add('hidden');
+        res.parentElement.previousElementSibling.classList.remove('hidden');
+        // разрешить редактировать другие заметки
+        allowEditingOfNotes(res);
+
+    })
+})
+
+// функция разрешить редактировать другие заметки
+function allowEditingOfNotes (userItem) {
+
+    const currentList = userItem.parentElement.parentElement;
+        
+    document.querySelectorAll('.list__item').forEach(function(list){
+        if(list !== currentList) list.querySelector('.list__wrapper-change ').classList.remove('hidden');
+    })
+}
